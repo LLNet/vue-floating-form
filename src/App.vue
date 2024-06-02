868e4fcaf6f53f607596c2e3e96a66406a2928ee
llnet <git@luislucas.dk>
2024-06-02 12:12:27 +0200

    Add more test in App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Select from "./components/Select.vue";
 import Textarea from "./components/Textarea.vue";
 import {ref} from "vue";
 import Number from "./components/Number.vue";
+import Editor from "./components/Editor.vue";
 
 const form = ref({
   name: '',
@@ -13,11 +14,14 @@ const form = ref({
 });
 </script>
 <template>
-  <div class="grid grid-cols-1 gap-4 p-4">
+  <div class="grid grid-cols-1 gap-4 p-4 container mx-auto">
     <Text v-model="form.name" />
     <Number v-model="form.number" />
-    <Select v-model="form.type" empty-value="" />
+    <Select v-model="form.type" empty-value="" :options="['1', {label:2, value:2}, {label:2, value:2, group:'test'}, {label:3}, '5']" />
+    <Select v-model="form.type" empty-value="" :options="{'1':'1', 2:{value:2, label:'2'}, 3:{value:'_3', label:'__3', group:'__4'}}" />
     <Textarea v-model.lazy="form.message" />
+    <Editor v-model="form.message" />
+    <Editor v-model="form.name" content-type="text" />
   </div>
   <pre>
     {{ form }}
