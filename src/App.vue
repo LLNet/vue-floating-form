c963f9ea7601f9b477a8b41e34bd9bf5dfeb8f46
llnet <git@luislucas.dk>
2024-06-03 08:06:43 +0200

    add more to test.

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,23 @@ import Textarea from "./components/Textarea.vue";
 import {ref} from "vue";
 import Number from "./components/Number.vue";
 import Editor from "./components/Editor.vue";
+import Select2 from "@/components/Select2.vue";
 
 const form = ref({
   name: '',
   type: '',
+  type2: [],
   message: '',
   number: 0
 });
 </script>
 <template>
-  <div class="grid grid-cols-1 gap-4 p-4 container mx-auto">
+  <div class="grid grid-cols-2 gap-4 p-4 container mx-auto">
     <Text v-model="form.name" />
     <Number v-model="form.number" />
-    <Select v-model="form.type" empty-value="" :options="['1', {label:2, value:2}, {label:2, value:2, group:'test'}, {label:3}, '5']" />
-    <Select v-model="form.type" empty-value="" :options="{'1':'1', 2:{value:2, label:'2'}, 3:{value:'_3', label:'__3', group:'__4'}}" />
+    <Select v-model="form.type" :empty-value="null" :options="['1', {label:2, value:2}, {label:2, value:2, group:'test'}, {label:3}, '5']" />
+    <Select2 v-model="form.type" :empty-value="null" />
+    <Select2 :multiple="true" v-model="form.type2" :empty-value="null" :options="['1', {label:2, value:2}, {label:2, value:2, group:'test'}, {label:3}, '5']" />
     <Textarea v-model.lazy="form.message" />
     <Editor v-model="form.message" />
     <Editor v-model="form.name" content-type="text" />
